6ea7c3b8f760f8c1b6fd70d7d2053f79479c0490
asifalif40 <2220031@iub.edu.bd>
2024-04-15 19:57:48 +0600

    Add

---
+++ Simulating_Operations_Of_Transparency_International_Bangladesh_Software/src/Alif/FinancialManagementController.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Alif;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author asifk
+ */
+public class FinancialManagementController implements Initializable {
+
+    @FXML
+    private Button summary;
+    @FXML
+    private Button draft;
+    @FXML
+    private Button latestTransactions;
+    @FXML
+    private Button messages;
+    @FXML
+    private Button export;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void summaryButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void draftButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void latestTransactionsButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void messagesButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void exportButtonMouseOnClicked(ActionEvent event) {
+    }
+    
+}
---
+++ Simulating_Operations_Of_Transparency_International_Bangladesh_Software/src/Alif/FinancialRegulationsController.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Alif;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author asifk
+ */
+public class FinancialRegulationsController implements Initializable {
+
+    @FXML
+    private Button regulatoryMonitoring;
+    @FXML
+    private Button reportAssessment;
+    @FXML
+    private Button controlsDevelopment;
+    @FXML
+    private Button training;
+    @FXML
+    private Button documentation;
+    @FXML
+    private Button oversight;
+    @FXML
+    private Button theImprovement;
+    @FXML
+    private Button auditCoordination;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void regulatoryMonitoringButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void reportAssessmentButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void controlsDevelopmentButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void trainingButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void documentationButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void oversightButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void theImprovementButtonMouseOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void auditCoordinationButtonMouseOnClicked(ActionEvent event) {
+    }
+    
+}
